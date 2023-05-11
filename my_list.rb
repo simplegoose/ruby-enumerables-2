253c9f536b527f5bf23557793e913717ea281b7b
Rashed Arman <rashedarman21@gmail.com>
2023-05-12 02:08:34 +0600

    feat: complete solution

---
+++ my_list.rb
@@ -0,0 +1,36 @@
+require './enumerable'
+
+class MyList
+  include MyEnumerable
+
+  def initialize(list)
+    @list = list
+  end
+
+  def each(&block)
+    # @list.each(&block) -- alternate approach
+    @list.each { |y| block.call(y) }
+  end
+end
+
+list = MyList.new([1, 2, 3, 4])
+
+# Test #all?
+
+p(list.all? { |e| e < 5 })
+# => true
+
+p(list.all? { |e| e > 5 })
+# => false
+
+# Test #any?
+
+p(list.any? { |e| e == 2 })
+# => true
+
+p(list.any? { |e| e == 5 })
+# => false
+
+# Test #filter?
+p(list.filter?(&:even?))
+# => [2, 4]
